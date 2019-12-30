ac6be347ec475297b8c76280e38e6f3bf8223774
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-12-30 17:13:50 -0500

    initial work experience box

--- src/scenes/Resume.js
+++ src/scenes/Resume.js
@@ -23,6 +23,30 @@ export default class Resume extends React.Component {
                                 I've always loved the fact that I can bring someone's thoughts to life by typing a few lines of code. I'm excited by entrepreneurship and making products to change the world..</p>
                         </div>
                     </div>
+                    <div className="card bg-light my-3">
+                        <div className="card-header">Work Experience</div>
+                        <div className="card-body">
+                            <h3 className="card-title">
+                                Railpod Inc Full Stack Web Developer- Boston, MA Jan. 2019 - Aug. 2019
+                            </h3>
+                            <p className="card-title">
+                                <ul>
+                                    <li>
+                                        Worked with the executive team to create major portions of the product
+                                    </li>
+                                    <li>
+                                        Created complex data visualization tools for track data using React that sped up the data analysis process by over 50%
+                                    </li>
+                                    <li>
+                                        Automated data uploading from robot to web portal using Django and AWS
+                                    </li>
+                                    <li>
+                                        Converted API that increased GPS accuracy from Java to Python to fit in with current codebase
+                                    </li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
 
                 </div>
                 <div className="col-2">
