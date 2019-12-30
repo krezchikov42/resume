470890f709dd4d98fad1c0d931a3786a844d44eb
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-12-30 15:38:53 -0500

    initial commit of Resume Website

---
+++ src/scenes/Resume.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class Resume extends React.Component {
+
+    render() {
+        return <div className="container-fluid">
+            <div className="row">
+                <div className="col bg-primary">
+                    Helo
+                </div>
+            </div>
+        </div>
+
+    }
+}
