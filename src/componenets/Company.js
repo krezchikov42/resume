1236f61d3bdcfe13325b8d0447d137773ab7affe
krezchikov42 <rezchikov.k@husky.neu.edu>
2020-01-02 13:07:57 -0500

    initial Company componenet

---
+++ src/componenets/Company.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class Company extends React.Component {
+    render() {
+        return <div>
+            <h3 className="card-title">
+                {this.props.job_title} - <span className="place">{this.props.company} </span> <span className={"year"}>{`(${this.props.year})`}</span>
+            </h3>
+            <p className="card-text">
+                <ul>
+                    {this.props.bullets.map((bullet,index) => <li key={index}>{bullet}</li>)}
+                </ul>
+            </p>
+        </div>
+    }
+}
