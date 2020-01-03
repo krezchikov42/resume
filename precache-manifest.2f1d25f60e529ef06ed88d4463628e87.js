7d8686414955eb4c636aa7ff8849fbc06bc32506
krezchikov42 <rezchikov.k@husky.neu.edu>
2020-01-03 12:43:38 -0500

    Updates

---
+++ precache-manifest.2f1d25f60e529ef06ed88d4463628e87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9895eae18ea858f28e490b3e6bec1f63",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "1efe3baa633e5907382b",
+    "url": "/resume/static/css/main.ba48c2ae.chunk.css"
+  },
+  {
+    "revision": "b40eaf7916f07974ecdf",
+    "url": "/resume/static/js/2.bd5aa231.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/resume/static/js/2.bd5aa231.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1efe3baa633e5907382b",
+    "url": "/resume/static/js/main.db8487e9.chunk.js"
+  },
+  {
+    "revision": "e3be495ebf93da27ea4e",
+    "url": "/resume/static/js/runtime-main.687946a0.js"
+  },
+  {
+    "revision": "04c267e74a2d3f25e24eaefb07eb26a8",
+    "url": "/resume/static/media/me.04c267e7.png"
+  }
+]);
